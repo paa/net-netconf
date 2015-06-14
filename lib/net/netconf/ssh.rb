69ebbfb8aca67307e503a9d5d7516157eb38b013
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-06-14 18:41:09 -0400

    Fix more rubocops warnings / errors

--- lib/net/netconf/ssh.rb
+++ lib/net/netconf/ssh.rb
@@ -1,40 +1,42 @@
 require 'net/ssh'
+require 'net/netconf/ssh'
 
 module Netconf
   class SSH < Netconf::Transport
-
     NETCONF_PORT = 830
     NETCONF_SUBSYSTEM = 'netconf'
 
-    def initialize( args_h, &block )
+    def initialize(args_h, &block)
       @args = args_h.clone
       @args[:os_type] = args_h[:os_type] || Netconf::DEFAULT_OS_TYPE
 
       # extend this instance with the capabilities of the specific os_type
       begin
-        extend Netconf::const_get( @args[:os_type] )::TransSSH
+        extend Netconf.const_get(@args[:os_type]).TransSSH
       rescue NameError
         # no extensions available ...
       end
 
-      @trans = Hash.new
-      super( &block )
+      @trans = {}
+      super(&block)
     end
 
-    def trans_open( &block )
+    def trans_open(&block)
       # open a connection to the NETCONF subsystem
-      start_args = Hash.new
+      start_args = {}
       start_args[:password] ||= @args[:password]
       start_args[:passphrase] = @args[:passphrase] || nil
       start_args[:port] = @args[:port] || NETCONF_PORT
       start_args.merge!(@args[:ssh_args]) if @args[:ssh_args]
 
       begin
-        @trans[:conn] = Net::SSH.start( @args[:target], @args[:username], start_args )
-        @trans[:chan] = @trans[:conn].open_channel{ |ch| ch.subsystem( NETCONF_SUBSYSTEM ) }
+        @trans[:conn] = Net::SSH.start(@args[:target], @args[:username], start_args)
+        @trans[:chan] = @trans[:conn].open_channel do |ch|
+          ch.subsystem(NETCONF_SUBSYSTEM)
+        end
       rescue Errno::ECONNREFUSED => e
         if self.respond_to? 'trans_on_connect_refused'
-          return trans_on_connect_refused( start_args )
+          return trans_on_connect_refused(start_args)
         end
         return nil
       end
@@ -54,9 +56,9 @@ module Netconf
       # the "on" functions must be set before calling
       # the #loop method
 
-      @trans[:chan].on_data do |ch, data|
-        if data.include?( RPC::MSG_END )
-          data.slice!( RPC::MSG_END )
+      @trans[:chan].on_data do |_ch, data|
+        if data.include?(RPC::MSG_END)
+          data.slice!(RPC::MSG_END)
           @trans[:rx_buf] << data unless data.empty?
           @trans[:more] = false
         else
@@ -65,7 +67,7 @@ module Netconf
       end
 
       # ... if there are errors ...
-      @trans[:chan].on_extended_data do |ch, type, data|
+      @trans[:chan].on_extended_data do |_ch, _type, data|
         @trans[:rx_err] = data
         @trans[:more] = false
       end
@@ -75,20 +77,19 @@ module Netconf
 
       @trans[:conn].loop { @trans[:more] }
 
-      return @trans[:rx_buf]
+      @trans[:rx_buf]
     end
 
-    def trans_send( cmd_str )
-      @trans[:chan].send_data( cmd_str )
+    def trans_send(cmd_str)
+      @trans[:chan].send_data(cmd_str)
     end
 
     # accessor to create an Net::SCP object so the caller can perform
     # secure-copy operations (see Net::SCP) for details
     def scp
-      @scp ||= Net::SCP.start( @args[:target], @args[:username], :password => @args[:password] )
+      @scp ||= Net::SCP.start(@args[:target],
+                              @args[:username],
+                              password: @args[:password])
     end
-
   end # class: SSH
-end #module: Netconf
-
-require 'net/netconf/ssh'
+end # module: Netconf
--- lib/net/netconf/telnet.rb
+++ lib/net/netconf/telnet.rb
@@ -1,36 +1,34 @@
 require 'net/telnet'
 
 module Netconf
-
   class Telnet < Netconf::Transport
-
-    def initialize( args, trans_args = nil, &block )
+    def initialize(args, trans_args = nil, &block)
       os_type = args[:os_type] || Netconf::DEFAULT_OS_TYPE
       @args = args.clone
 
       # extend this instance with the capabilities of the specific console
       # type; it needs to define #login and #start_netconf session
       begin
-        extend Netconf::const_get( os_type )::TransTelnet
+        extend Netconf.const_get(os_type).TransTelnet
       rescue NameError
         # no extensions available ...
       end
 
       my_trans_args = {}
-      my_trans_args["Host"] = @args[:target]
-      my_trans_args["Port"] = @args[:port] if @args[:port]
+      my_trans_args['Host'] = @args[:target]
+      my_trans_args['Port'] = @args[:port] if @args[:port]
 
-      @trans = Net::Telnet.new( my_trans_args )
+      @trans = Net::Telnet.new(my_trans_args)
 
       @trans_timeout = @args[:timeout] || Netconf::DEFAULT_TIMEOUT
       @trans_waitio = @args[:waitio] || Netconf::DEFAULT_WAITIO
 
-      super( &block )
+      super(&block)
     end
 
-    def trans_open( &block )
-      trans_login()
-      trans_start_netconf()
+    def trans_open(&block)
+      trans_login
+      trans_start_netconf
       self
     end
 
@@ -39,14 +37,13 @@ module Netconf
       @trans.close
     end
 
-    def trans_send( cmd_str )
-      @trans.write( cmd_str )
+    def trans_send(cmd_str)
+      @trans.write cmd_str
     end
 
     def trans_receive
-      rsp = @trans.waitfor( Netconf::RPC::MSG_END_RE )
-      rsp.chomp!( Netconf::RPC::MSG_END + "\n" )
+      rsp = @trans.waitfor(Netconf::RPC::MSG_END_RE)
+      rsp.chomp!(Netconf::RPC::MSG_END + "\n")
     end
-
   end # class: Serial
 end # module: Netconf
